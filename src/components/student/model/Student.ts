36f5e66ccb2cba355d727615f826a74bdbdecf5f
Ali Hadi Öztürk <alihadi06@gmail.com>
2020-12-26 02:11:18 +0300

    student names changed

--- src/components/student/model/Student.ts
+++ src/components/student/model/Student.ts
@@ -18,14 +18,14 @@ export interface Person {
 
 
 export const Students: Student[] = [
-    { id: 3, schoolNumber: '160910112', person: { nationalId: '11111111111', name: 'Dilara', surname: 'Yoldaş' } },
+    { id: 3, schoolNumber: '160910112', person: { nationalId: '11111111111', name: 'Jess', surname: 'Chestain' } },
     {
         id: 1, schoolNumber: '160910111', person: { nationalId: '22222222222', name: 'Ali hadi', surname: 'Öztürk' }, lessons: [
             { id: 1, description: 'Computer engineering lesson', name: 'Computer engineering' },
             { id: 3, description: 'Learning react full included', name: 'React' }
         ]
     },
-    { id: 2, schoolNumber: '160910109', person: { nationalId: '33333333333', name: 'Özgür', surname: 'Çınar' } },
+    { id: 2, schoolNumber: '160910109', person: { nationalId: '33333333333', name: 'Nikola', surname: 'Tesla' } },
 ]
 
 
